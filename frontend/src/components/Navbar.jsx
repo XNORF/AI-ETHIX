59769d5c03f18b4d3c7e79dccbf1a741b8bb2eb5
XNORF <78629634+XNORF@users.noreply.github.com>
2024-04-14 19:13:44 +0800

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
-import Modal from "@mui/material/Modal";
+import { Modal, Avatar, Stack } from "@mui/material";
 
 import Feedback from "./Feedback";
 
@@ -13,47 +12,33 @@ const Navbar = () => {
     return (
         <>
             <div className="nav">
-                <ul>
-                    <li>
-                        <Link to="/" style={{ textDecoration: "none" }}>
-                            <p>
-                                <b>AI-ETHIX</b>
-                            </p>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/resources" style={{ textDecoration: "none" }}>
-                            <p>Resources</p>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/guidelines" style={{ textDecoration: "none" }}>
-                            <p>Guidelines</p>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/forums" style={{ textDecoration: "none" }}>
-                            <p>Forums</p>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/quiz" style={{ textDecoration: "none" }}>
-                            <p>Quizzes</p>
-                        </Link>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <Link onClick={handleOpen} style={{ textDecoration: "none" }}>
-                            <ChatBubbleIcon />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/profile" style={{ textDecoration: "none" }}>
-                            <AccountCircleIcon />
-                        </Link>
-                    </li>
-                </ul>
+                <Stack direction="row" spacing={2}>
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                        <p>
+                            <b>AI-ETHIX</b>
+                        </p>
+                    </Link>
+                    <Link to="/resources" style={{ textDecoration: "none" }}>
+                        <p>Resources</p>
+                    </Link>
+                    <Link to="/guidelines" style={{ textDecoration: "none" }}>
+                        <p>Guidelines</p>
+                    </Link>
+                    <Link to="/forums" style={{ textDecoration: "none" }}>
+                        <p>Forums</p>
+                    </Link>
+                    <Link to="/quiz" style={{ textDecoration: "none" }}>
+                        <p>Quizzes</p>
+                    </Link>
+                </Stack>
+                <Stack direction="row" spacing={2}>
+                    <Link onClick={handleOpen} style={{ textDecoration: "none" }}>
+                        <ChatBubbleIcon />
+                    </Link>
+                    <Link to="/profile" style={{ textDecoration: "none" }}>
+                        <Avatar sx={{ width: 24, height: 24 }} />
+                    </Link>
+                </Stack>
             </div>
             <Modal open={open} onClose={handleClose}>
                 <Feedback />
